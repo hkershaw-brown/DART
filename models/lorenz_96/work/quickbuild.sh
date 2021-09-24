08b9e0e297e3ff044197eb1ff2093e3b5f1da69c
Helen Kershaw <hkershaw@ucar.edu>
2021-09-24 10:07:02 -0600

    quickbuild

--- models/lorenz_96/work/quickbuild.sh
+++ models/lorenz_96/work/quickbuild.sh
@@ -1,22 +1,37 @@
 #!/bin/bash -v
 
+set -e
+
+[ -z "$DART2" ] && echo "ERROR: Must set DART environment variable"; exit 9
+
 # Build for DART programs
 
 function buildit() {
- $DART/build_templates/mkmf -p $1 -a $DART $DART/src/programs/$1 \
+ $DART/build_templates/mkmf -p $1 -a $DART \ $DART/src/programs/$1/path_names_$1 \
+     $DART/src/programs/$1 \
      $DART/src/core \
-     $DART/src/location \
+     $DART/src/location/oned \
+     $DART/src/location/utilities \
      $DART/src/null_mpi \
+     .. \
+     $DART/models/utilities/default_model_mod.f90 \
+     $DART/observations/forward_operators/obs_def_mod.f90 \
+     $DART/observations/forward_operators/obs_def_utilities_mod.f90 \
+     $DART/src/model_mod_tools/test_interpolate_oned.f90
 
  make $1
+}
+
+function buildpreprocess() {
+ $DART/build_templates/mkmf -p preprocess -a $DART $DART/src/programs/preprocess/path_names_preprocess
+ make
 }
 
 # clean the directory
 \rm -f *.o *.mod Makefile .cppdefs
 
 # build and run preprocess before making any other DART executables
-buildit preprocess
-exit
+buildpreprocess
 ./preprocess
 
 programs=( \
@@ -28,7 +43,7 @@ filter \
 integrate_model \
 model_mod_check \
 obs_common_subset \
-obs_diag \
+#obs_diag \
 obs_sequence_tool \
 perfect_model_obs \
 )
@@ -39,7 +54,4 @@ for p in ${programs[@]}; do
   buildit $p
   ((i++))
 done
-
-
-
 
