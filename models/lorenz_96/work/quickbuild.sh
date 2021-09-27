69b2df207892c51086f5421d28ef91b6437d1730
Helen Kershaw <hkershaw@ucar.edu>
2021-09-27 16:02:44 -0600

    compile single program

--- models/lorenz_96/work/quickbuild.sh
+++ models/lorenz_96/work/quickbuild.sh
@@ -7,15 +7,6 @@ set -e
 
 source $DART/build_templates/buildfunctions.sh
 
-# clean the directory
-\rm -f *.o *.mod Makefile .cppdefs
-
-# DART source files
-findsrc oned
-
-# build and run preprocess before making any other DART executables
-buildpreprocess
-
 programs=( \
 closest_member_tool \
 create_fixed_network_seq \
@@ -25,10 +16,40 @@ filter \
 integrate_model \
 model_mod_check \
 obs_common_subset \
-#obs_diag \
+obs_diag \
 obs_sequence_tool \
 perfect_model_obs \
 )
+
+model_programs=(\
+)
+
+# clean the directory
+\rm -f *.o *.mod Makefile .cppdefs
+
+# DART source files
+findsrc oned
+
+# build and run preprocess before making any other DART executables
+buildpreprocess
+
+if [ ! -z "$1" ] ; then
+    if [[ " ${programs[*]} " =~ " ${1} " ]]; then
+       # whatever you want to do when array contains value
+       echo "building " $1
+       buildit $1
+       exit
+    elif [[ " ${model_programs[*]} " =~ " ${1} " ]];then 
+       echo "building " $1
+       build $1
+       exit
+    else
+       echo "ERROR: unknown program" $1
+    fi
+else 
+   echo "no args"
+fi
+exit
 
 i=1
 for p in ${programs[@]}; do
