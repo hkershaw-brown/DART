90c7ef171599f616403a515cac05f00c22e5acbc
Helen Kershaw <hkershaw@ucar.edu>
2021-09-27 12:58:40 -0600

    function for finding src

--- models/wrf/work/quickbuild.sh
+++ models/wrf/work/quickbuild.sh
@@ -9,20 +9,7 @@ set -e
 \rm -f *.o *.mod Makefile .cppdefs
 
 # DART source files
-core=$(find $DART/src/core -type f -name "*.f90") 
-modelsrc=$(find ../src -type d -name programs -prune -o -type f -name "*.f90" -print)
-
-
-location="$DART/src/location/threed_sphere \
-          $DART/src/model_mod_tools/test_interpolate_threed_sphere.f90"
-
-misc="$DART/src/location/utilities \
-     $DART/src/null_mpi \
-     $DART/models/utilities/default_model_mod.f90 \
-     $DART/observations/forward_operators/obs_def_mod.f90 \
-     $DART/observations/forward_operators/obs_def_utilities_mod.f90"
-
-join="${core} ${modelsrc} ${misc} ${location}"
+findsrc
 
 # build and run preprocess before making any other DART executables
 buildpreprocess
@@ -78,28 +65,44 @@ done
 
 }
 
+#
+function findsrc() {
+
+local core=$(find $DART/src/core -type f -name "*.f90") 
+local modelsrc=$(find ../src -type d -name programs -prune -o -type f -name "*.f90" -print)
+local location="$DART/src/location/threed_sphere \
+                $DART/src/model_mod_tools/test_interpolate_threed_sphere.f90"
+
+local misc="$DART/src/location/utilities \
+            $DART/src/null_mpi \
+            $DART/models/utilities/default_model_mod.f90 \
+            $DART/observations/forward_operators/obs_def_mod.f90 \
+            $DART/observations/forward_operators/obs_def_utilities_mod.f90"
+
+dartsrc="${core} ${modelsrc} ${misc} ${location}"
+}
+
 #-------------------------
 # Build a program 
 # Arguements: 
 #  program name
 # Globals:
 #  DART - root of DART
-#  core - directory containing core DART source code
+#  dartsrc - source files
 #-------------------------
 function buildit() {
  $DART/build_templates/mkmf -p $1 \
      $DART/src/programs/$1 \
-     $join
+     $dartsrc
  make $1
 }
 
 
 function build() {
  $DART/build_templates/mkmf -p $1 ../src/programs/$1.f90 \
-     $join
+     $dartsrc
  make $1
 }
-
 
 #-------------------------
 # Build and run preprocess
