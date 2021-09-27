eede850d55cee9e0fe599effe8b9112e0072fe51
Helen Kershaw <hkershaw@ucar.edu>
2021-09-27 09:30:58 -0600

    main function for quickbuild

--- models/lorenz_96/work/quickbuild.sh
+++ models/lorenz_96/work/quickbuild.sh
@@ -1,36 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 
+main() {
 set -e
 
 [ -z "$DART" ] && echo "ERROR: Must set DART environment variable" && exit 9
-
-#----------------------
-# Functions
-#----------------------
-function buildit() {
- $DART/build_templates/mkmf -v -p $1 -a $DART $DART/src/programs/$1/path_names_$1 \
-     $DART/src/programs/$1 \
-     $core \
-     $DART/src/location/oned \
-     $DART/src/location/utilities \
-     $DART/src/null_mpi \
-     .. \
-     $DART/models/utilities/default_model_mod.f90 \
-     $DART/observations/forward_operators/obs_def_mod.f90 \
-     $DART/observations/forward_operators/obs_def_utilities_mod.f90 \
-     $DART/src/model_mod_tools/test_interpolate_oned.f90
-
- make $1
-}
-
-function buildpreprocess() {
- $DART/build_templates/mkmf -p preprocess -a $DART $DART/src/programs/preprocess/path_names_preprocess
- make
-}
-
-#----------------------
-# Build DART
-#---------------------
 
 # clean the directory
 \rm -f *.o *.mod Makefile .cppdefs
@@ -40,7 +13,6 @@ core=$(find $DART/src/core -type f -name "*.f90")
 
 # build and run preprocess before making any other DART executables
 buildpreprocess
-./preprocess
 
 programs=( \
 closest_member_tool \
@@ -66,3 +38,43 @@ done
 # clean up
 \rm -f *.o *.mod
 
+}
+
+#-------------------------
+# Build a program 
+# Arguements: 
+#  program name
+# Globals:
+#  DART - root of DART
+#  core - directory containing core DART source code
+#-------------------------
+function buildit() {
+ $DART/build_templates/mkmf -v1 -p $1 -a $DART $DART/src/programs/$1/path_names_$1 \
+     $DART/src/programs/$1 \
+     $core \
+     $DART/src/location/oned \
+     $DART/src/location/utilities \
+     $DART/src/null_mpi \
+     .. \
+     $DART/models/utilities/default_model_mod.f90 \
+     $DART/observations/forward_operators/obs_def_mod.f90 \
+     $DART/observations/forward_operators/obs_def_utilities_mod.f90 \
+     $DART/src/model_mod_tools/test_interpolate_oned.f90
+
+ make $1
+}
+
+#-------------------------
+# Build and run preprocess
+# Arguements: 
+#  none
+# Globals:
+#  DART - root of DART
+#-------------------------
+function buildpreprocess() {
+ $DART/build_templates/mkmf -p preprocess -a $DART $DART/src/programs/preprocess/path_names_preprocess
+ make
+ ./preprocess
+}
+
+main "$@"
