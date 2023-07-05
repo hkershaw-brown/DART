7c666606338c1eb66bda83e171544f7044853419
Helen Kershaw <hkershaw@ucar.edu>
2023-07-05 10:38:48 -0400

    run script setup

--- models/tiegcm/shell_scripts/experiment.py
+++ models/tiegcm/shell_scripts/experiment.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 class Experiment:
     """ TIEGCM experiment """
-    model = 'TIEGCM'
+    model = "TIEGCM"
 
 
     def __init__(self, root, data, account, resolution, cycle_delta, initial_time):
@@ -16,7 +16,25 @@ class Experiment:
     def info(self):
         pprint(vars(self))
         
+    def setup_tiegcm_run(self, pbs_template):
+        readFile = open("batch_script_templates/run-tiegcm.pbs.template")
+        data = readFile.read()
+        data = data.replace("{res}", str(self.resolution))
+        data = data.replace("{account}", self.account)
+        data = data.replace("{tgcmdata}", self.data)
+        data = data.replace("{tiegcm_root}", self.root)
         
+    
+        if self.resolution == 2.5:
+             nodes = "select=1:ncpus=32:mpiprocs=32:ompthreads=1"
+        else:
+            nodes = "select=1:ncpus=16:mpiprocs=16:ompthreads=1"
+    
+        data = data.replace("{nodes}", nodes)
+    
+        writeFile = open(pbs_template, "w")
+        writeFile.write(data)
+
 class Pmo(Experiment):
     """ Perfect Model Obs experiment """
     
@@ -26,9 +44,29 @@ class Pmo(Experiment):
                 
        
 class Filter(Experiment):
-     """ Filter experiment """
+    """ Filter experiment """
      
-     def __init__(self, root, data, account, resolution, cycle_delta, initial_time, obs_seq_out, ens_size):
-         super().__init__(root, data, account, resolution, cycle_delta, initial_time)
-         self.obs_seq_out = obs_seq_out
-         self.ens_size = ens_size
+    def __init__(self, root, data, account, resolution, cycle_delta, initial_time, obs_seq_out, ens_size):
+        super().__init__(root, data, account, resolution, cycle_delta, initial_time)
+        self.obs_seq_out = obs_seq_out
+        self.ens_size = ens_size
+         
+    def setup_tiegcm_run(self, pbs_template):
+        readFile = open("batch_script_templates/run-array-tiegcm.pbs.template")
+        data = readFile.read()
+        data = data.replace("{res}", str(self.resolution))
+        data = data.replace("{account}", self.account)
+        data = data.replace("{tgcmdata}", self.data)
+        data = data.replace("{tiegcm_root}", self.root)
+        data = data.replace("{ens_size}", str(self.ens_size))
+    
+        if self.resolution == 2.5:
+             nodes = "select=1:ncpus=32:mpiprocs=32:ompthreads=1"
+        else:
+            nodes = "select=1:ncpus=16:mpiprocs=16:ompthreads=1"
+    
+        data = data.replace("{nodes}", nodes)
+    
+        writeFile = open(pbs_template, "w")
+        writeFile.write(data)
+
