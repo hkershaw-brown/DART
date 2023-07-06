f62dec0c34cd2c5fdc78a38a0d34ade399b8e1e6
Helen Kershaw <hkershaw@ucar.edu>
2023-07-06 14:15:10 -0400

    use super.setup for filter experiments

--- models/tiegcm/shell_scripts/experiment.py
+++ models/tiegcm/shell_scripts/experiment.py
@@ -12,18 +12,38 @@ class Experiment:
  
 
     def __init__(self, root, data, account, resolution, cycle_delta, initial_time):
+       """ Initialize an experiment
+       
+       root -- TIEGCM root directory
+       data -- TIEGCM data directory
+       resolution -- TIEGCM resolution 2.5 or 5.0 degrees
+       cycle_deta -- how often to stop TIEGCM
+       initial_time -- start time of the experiment
+       
+       """
        self.root = root 
        self.data = data 
        self.account = account 
        self.resolution = resolution 
        self.cycle_delta = cycle_delta
-       self.inital_time = initial_time
+       self.initial_time = initial_time
+       self.exp_directory = ""
+       self.tiegcm_pbs_template = "run-tiegcm.pbs.template"
 
     def info(self):
+        """ Print out all the variables of the experiment """
         pprint(vars(self))
         
-    def setup_pbs_options_tiegcm(self, pbs_template, pbs_file):
-        """ Create PBS files for tiegcm run """
+    def create_experiment_directory(self):
+        try:
+            os.makedirs(self.exp_directory)
+        except FileExistsError:
+            print("Experiment directory {} exists".format(self.exp_directory))
+            sys.exit()
+    
+        
+    def setup_pbs_options(self, pbs_template, pbs_file):
+        """ Create PBS files from a template """
         readFile = open(os.path.join(self.batch_script_templates_dir, pbs_template))
         data = readFile.read()
         readFile.close()
@@ -46,19 +66,20 @@ class Experiment:
     def setup(self, directory):
         """ Setup a cycling experiment for TIEGCM
             directory - to be created for the experiment
+            
+            creates:
+              directory to run experiment
+              PBS script to run tiegcm
         """
-        try:
-            os.makedirs(directory)
-        except FileExistsError:
-            print("Experiment directory {} exists".format(directory))
-            sys.exit()
-        pbs_file = os.path.join(directory, "run-tiegcm.pbs")
-        self.setup_pbs_options_tiegcm("run-tiegcm.pbs.template", pbs_file)
+        self.exp_directory = directory
+        self.create_experiment_directory()
+        pbs_file = os.path.join(self.exp_directory, self.tiegcm_pbs_file)
+        self.setup_pbs_options(self.tiegcm_pbs_template, pbs_file)
       
 
     def run(self, num_cycles):
         """ Submit tiegcm jobs """
-        result = subprocess.run(['qsub', 'submit.sh'], stdout=subprocess.PIPE)
+        result = subprocess.run(['qsub', self.tiegcm_pbs_file], stdout=subprocess.PIPE)
         model_run = f"depend=afterok:{result.stdout.strip().decode('utf8')}"
 
         for cycle in range(num_cycles):
@@ -79,26 +100,24 @@ class Pmo(Experiment):
 class Filter(Experiment):
     """ Filter experiment """
     filter_pbs_file = "submit_filter.pbs"
+    assim_dir = "assim"
      
     def __init__(self, root, data, account, resolution, cycle_delta, initial_time, obs_seq_out, ens_size):
         super().__init__(root, data, account, resolution, cycle_delta, initial_time)
         self.obs_seq_out = obs_seq_out
         self.ens_size = ens_size
+        self.tiegcm_pbs_template = "run-array-tiegcm.pbs.template"
         
          
     def setup(self, directory):
         """ Setup a Data Assimilation experiment for TIEGCM
+        
             directory - to be created for the experiment
         """
-        try:
-            os.makedirs(directory)
-            self.directory = directory
-        except FileExistsError:
-            print("Experiment directory {} exists".format(directory))
-            sys.exit()
-        pbs_file = os.path.join(self.directory, self.tiegcm_pbs_file)
+        super().setup(directory)
         
-        self.setup_pbs_options_tiegcm("run-array-tiegcm.pbs.template", pbs_file)
+        # Ensemble size for the number of array jobs
+        pbs_file = os.path.join(self.exp_directory, self.tiegcm_pbs_file)
         readFile = open(pbs_file)
         data = readFile.read()
         readFile.close()
@@ -110,16 +129,21 @@ class Filter(Experiment):
         self.setup_members()
         self.setup_filter_pbs()
         
+        # Create directory for assimilation
+        os.makedirs(os.path.join(self.exp_directory, self.assim_dir))
+        
+        self.info()
+        
     def setup_filter_pbs(self):
-        pbs_file = os.path.join(self.directory, self.filter_pbs_file)
-        self.setup_pbs_options_tiegcm("submit_filter.pbs.template", pbs_file)
+        pbs_file = os.path.join(self.exp_directory, self.filter_pbs_file)
+        self.setup_pbs_options("submit_filter.pbs.template", pbs_file)
         
     def setup_members(self):
         [ self.copy_mem(x) for x in range(self.ens_size)]
 
     def copy_mem(self, x):
         mem = "mem{:03d}".format(x)
-        shutil.copytree("mem.setup", os.path.join(self.directory, mem))
+        shutil.copytree("mem.setup", os.path.join(self.exp_directory, mem))
         
     def run(self, num_cycles):
         """ Submit filter experiment """
--- models/tiegcm/shell_scripts/run_experiment.py
+++ models/tiegcm/shell_scripts/run_experiment.py
@@ -1,19 +1,19 @@
 from experiment import Experiment, Pmo, Filter
 
 
-#ex1 = Experiment('/root/dir', '/data/dir', '23423', 5.0, 1, 0)
+ex1 = Experiment('/root/dir', '/data/dir', '23423', 2.5, 1, 0)
 
-#ex2 = Pmo('/root/dir', '/data/dir', '23423', 5.0, 1, 0, 'obs_seq.in')
+ex2 = Pmo('/root/dir', '/data/dir', '23423', 5.0, 1, 0, 'obs_seq.in')
 
 ex3 = Filter('/root/dir', '/data/dir', '23423', 5.0, 1, 0, 'obs_seq.out', 80)
 
 #ex1.info()
 #ex2.info()
-ex3.info()
+#ex3.info()
 
 
-#ex1.setup("Cycle")
-#ex2.setup("PMO")
+ex1.setup("Cycle")
+ex2.setup("PMO")
 ex3.setup("Assimilation")
 
 #ex1.run(4)
