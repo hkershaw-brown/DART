058ddb1ddc527527c0e77f49b746d66936a58708
Helen Kershaw <hkershaw@ucar.edu>
2023-08-25 14:44:38 -0600

    grouped 2.5 vs 5.0 options together

--- models/tiegcm/shell_scripts/run_experiment.py
+++ models/tiegcm/shell_scripts/run_experiment.py
@@ -4,23 +4,24 @@ from experiment import FreeRun, PerfectModelObs, Filter
 tiegcm_root = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm2.0/'
 
 # 5.0 executable and data
-#tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec5.0/tiegcm.exe'
-#tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
+tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec5.0/tiegcm.exe'
+tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
+resolution = 5.0 # degrees
+delta_time = 1 # hours (decimal 0.5 for 30 minutes)
 
 # 2.5 degree is a different executable and data
-tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec2.5/tiegcm.exe'
-tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res2.5_data'
+#tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec2.5/tiegcm.exe'
+#tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res2.5_data'
+#resolution = 2.5 # degrees
+#delta_time = 0.5 # hours (decimal 0.5 for 30 minutes)
 
 #tiegcm_root = '/Users/hkershaw/DART/Projects/TIEGCM/fake_tiegcm_root/'
 #tiegcm_exe = '/Users/hkershaw/DART/Projects/TIEGCM/fake_tiegcm_root/tiegcm.exe'
 #tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
+
 account = 'P86850054'
-#resolution = 5.0 # degrees
-resolution = 2.5 # degrees
-delta_time = 0.5 # hours (decimal 0.5 for 30 minutes)
 intial_time = '2002-03-21 00:00:00'
 end_time = '2002-03-21 02:00:00'
-
 
 ex1 = FreeRun(tiegcm_root, tiegcm_exe, tiegcm_data, account, resolution, delta_time, intial_time, end_time, 'free_run')
 
@@ -36,7 +37,7 @@ ex1.info()
 #ex3.info()
 
 
-ex1.setup("Cycle_2.5")
+ex1.setup("Cycle")
 #ex2.setup("PMO")
 #ex3.setup("Assimilation")
 
