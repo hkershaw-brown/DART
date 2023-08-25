7d768edd75f3524bf8de5da452fd3044afad1dca
Helen Kershaw <hkershaw@ucar.edu>
2023-08-25 07:34:35 -0600

    fix: pbs templates for 2.5 history file write frequency
    This needs to be delta. How is delta different for 2.5 vs 5 degress?
    1 hour for 5 degrees, what should 2.5 degrees be

--- models/tiegcm/shell_scripts/run_experiment.py
+++ models/tiegcm/shell_scripts/run_experiment.py
@@ -2,14 +2,21 @@ from experiment import FreeRun, PerfectModelObs, Filter
 
 # Cheyenne
 tiegcm_root = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm2.0/'
-tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec5.0/tiegcm.exe'
-tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
+
+# 5.0 executable and data
+#tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec5.0/tiegcm.exe'
+#tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
+
+# 2.5 degree is a different executable and data
+tiegcm_exe = '/glade/work/hkershaw/tiegcm/tiegcm-cheyenne/tiegcm.exec2.5/tiegcm.exe'
+tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res2.5_data'
 
 #tiegcm_root = '/Users/hkershaw/DART/Projects/TIEGCM/fake_tiegcm_root/'
 #tiegcm_exe = '/Users/hkershaw/DART/Projects/TIEGCM/fake_tiegcm_root/tiegcm.exe'
 #tiegcm_data = '/glade/work/hkershaw/tiegcm/tiegcm-data/tiegcm_res5.0_data'
 account = 'P86850054'
-resolution = 5.0 # degrees
+#resolution = 5.0 # degrees
+resolution = 2.5 # degrees
 delta_time = 1 # hour
 intial_time = '2002-03-21 00:00:00'
 end_time = '2002-03-21 02:00:00'
@@ -24,12 +31,12 @@ ex1 = FreeRun(tiegcm_root, tiegcm_exe, tiegcm_data, account, resolution, delta_t
 #ens_size = 20
 #ex3 = Filter(tiegcm_root, tiegcm_exe, tiegcm_data, account, resolution, delta_time, intial_time, end_time, obs_seq_dir, ens_size)
 
-#ex1.info()
+ex1.info()
 #ex2.info()
 #ex3.info()
 
 
-ex1.setup("Cycle")
+ex1.setup("Cycle_2.5")
 #ex2.setup("PMO")
 #ex3.setup("Assimilation")
 
