721a48b75eafa8e55526264c5d611c83ef423da0
Helen Kershaw <hkershaw@ucar.edu>
2025-08-21 15:05:54 -0400

    script to run multiple runs

---
+++ developer_tests/get_close/work/run_many_obs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to run test_get_close with different my_num_state values
+
+# List of my_num_state values to test
+#state_list=(10 100 1000 10000 100000 1000000 10000000 20000000 40000000 80000000 100000000 200000000 400000000 800000000 1000000000)
+state_list=(10 100 1000 10000 100000 1000000 10000000)
+
+for state in "${state_list[@]}"; do
+    echo "Running test_get_close with my_num_state=$state"
+    ./test_get_close $state 2>> temp.out
+    echo "---"
+done
