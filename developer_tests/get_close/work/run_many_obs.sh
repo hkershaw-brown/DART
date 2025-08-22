57cdcc90e1986f362625a89f3573e8a1c82f2c20
Helen Kershaw <hkershaw@ucar.edu>
2025-08-22 09:30:20 -0400

    add program name to temp.out

--- developer_tests/get_close/work/run_many_obs.sh
+++ developer_tests/get_close/work/run_many_obs.sh
@@ -3,13 +3,20 @@
 
 # List of my_num_state values to test
 #state_list=(10 100 1000 10000 100000 1000000 10000000 20000000 40000000 80000000 100000000 200000000 400000000 800000000 1000000000)
-state_list=(10 100 1000 10000 100000 1000000 10000000 20000000 40000000 80000000)
-#state_list=(10 100 1000 10000 100000 1000000 10000000)
+#state_list=(10 100 1000 10000 100000 1000000 10000000 20000000 40000000 80000000)
+state_list=(10 100 1000 10000 100000 1000000 10000000)
 num_obs=1000
 
+# Check for executable argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <executable>" >&2
+    exit 1
+fi
+exe=$1
+
 for state in "${state_list[@]}"; do
-    echo "Running test_get_close with my_num_state=$state"
+    echo "Running $exe with my_num_state=$state"
     #                num_state elements, num obs
-    ./test_get_close $state $num_obs 2>> temp.out
+    ./$exe $state $num_obs 2>> $exe.out
     echo "---"
 done
