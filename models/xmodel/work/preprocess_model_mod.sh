754fe52619eac3252696373d840e9c2d65b43435
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-02-25 15:27:14 -0500

    remove preprocess routine renmae, not working and wrong thing to do

--- models/xmodel/work/preprocess_model_mod.sh
+++ models/xmodel/work/preprocess_model_mod.sh
@@ -29,48 +29,12 @@ mkdir -p "$(dirname "$OUTPUT_PATH")"
 echo "Preprocessing $MODEL_MOD_PATH for model: $MODEL_NAME"
 echo "Output will be written to: $OUTPUT_PATH"
 
-# List of standard model_mod routines that need to be renamed
-# These are the required public interfaces
-ROUTINES=(
-  "static_init_model"
-  "get_model_size"
-  "get_state_meta_data"
-  "shortest_time_between_assimilations"
-  "model_interpolate"
-  "nc_write_model_atts"
-  "nc_write_model_vars"
-  "write_model_time"
-  "read_model_time"
-  "end_model"
-  "pert_model_copies"
-  "get_close_obs"
-  "get_close_state"
-  "convert_vertical_obs"
-  "convert_vertical_state"
-  "adv_1step"
-  "init_time"
-  "init_conditions"
-)
-
 # Start with the original file
 cp "$MODEL_MOD_PATH" "$OUTPUT_PATH"
 
 # Replace module name: module model_mod -> module {model}_model_mod
 sed -i.bak "s/^module model_mod$/module ${MODEL_NAME}_model_mod/g" "$OUTPUT_PATH"
 sed -i.bak "s/^end module model_mod$/end module ${MODEL_NAME}_model_mod/g" "$OUTPUT_PATH"
-
-# For each routine, add prefix to:
-# 1. Function/subroutine definitions
-# 2. Public declarations
-# 3. Calls within the module (if any)
-for routine in "${ROUTINES[@]}"; do
-  # Replace subroutine/function definitions
-  sed -i.bak "s/\(^\s*\)\(subroutine\|function\)\s\+${routine}\s*(/\1\2 ${MODEL_NAME}_${routine}(/g" "$OUTPUT_PATH"
-  sed -i.bak "s/\(^\s*end\s\+\)\(subroutine\|function\)\s\+${routine}\s*$/\1\2 ${MODEL_NAME}_${routine}/g" "$OUTPUT_PATH"
-  
-  # Replace in public declarations
-  sed -i.bak "s/\(public\s*::\s*\)${routine}\([\s,&]\)/\1${MODEL_NAME}_${routine}\2/g" "$OUTPUT_PATH"
-done
 
 # Clean up backup files
 rm -f "${OUTPUT_PATH}.bak"
